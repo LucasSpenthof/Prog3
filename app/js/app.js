f1454634a97f7de2600651a840942bf6b45e17b4
LucasSpenthof <lucas.spenthof@gmail.com>
2021-04-29 18:48:30 -0400

    Mudularizacao das funcoes do programa

--- app/js/app.js
+++ app/js/app.js
@@ -1,62 +1,59 @@
+import  getData  from "./api.js";
+
 const apiData = document.querySelector('.api-data')
 const spinner = document.querySelector('.spinner-grow')
 const nivelFilter = document.querySelector('.nivel-filter')
 
-spinner.style.display="none"
-async function getCursos(){
-    const url = "http://localhost:3000/cursos"
-    spinner.style.display="block"
-    const response = await axios.get(url)
-    spinner.style.display="none"
-    const cursosList = Array.from(response.data)
+showSpinner(false)
+
+function showSpinner(isShow=false){
+    if(isShow){
+        spinner.style.display="block"
+        return
+    }
+        spinner.style.display="none" 
+}
+
+function renderCards(cursosList){
     cursosList.forEach(async function(cursos){
         apiData.innerHTML+=`
-            <div class="card m-2" style="width:220px">
-                <section class="card-body">
-                    <h5 class="card-title">${cursos.curso}</h5>
-                    <p>
-                        Nivel: ${cursos.nivel}
-                    </p>
-                    <p>
-                        Duracao: ${cursos.duracao}
-                    </p>
-                    <p>
-                        Municipio: ${cursos.municipio}
-                    </p>
-                </section>
-            </div>
+        <div class="card m-2" style="width:220px">
+            <section class="card-body">
+                <h5 class="card-title">${cursos.curso}</h5>
+                <p> 
+                    Nivel: ${cursos.nivel}
+                </p>
+                <p>
+                    Duracao: ${cursos.duracao}
+                </p>
+                <p>
+                    Municipio: ${cursos.municipio}
+                </p>
+            </section>
+        </div>
         `
-    })   
+    }) 
+
 }
+
+async function getCursos(){
+    showSpinner(true)
+    const response = await getData('cursos')
+    showSpinner(false)
+    const cursosList = Array.from(response.data)
+    renderCards(cursosList)
+}
+
 async function search(query){
-    const url =  `http://localhost:3000/cursos?q=${query}`
-   
-    
-    const response = await axios.get(url);
+    showSpinner(true)
+    const response = await getData(`cursos?q=${query}`)
+    showSpinner(false)
     const cursosList = Array.from(response.data)
     apiData.innerHTML=""
-    cursosList.forEach(function(cursos){
-        apiData.innerHTML+=`
-        <div class="card m-2" style="width:220px">
-        <section class="card-body">
-            <h5 class="card-title">${cursos.curso}</h5>
-            <p>
-                Nivel: ${cursos.nivel}
-            </p>
-            <p>
-                Duracao: ${cursos.duracao}
-            </p>
-            <p>
-                Municipio: ${cursos.municipio}
-            </p>
-        </section>
-    </div>
-          `
-    })
+    renderCards(cursosList)
 }
 async function getNivel(){
-    const url =`http://localhost:3000/nivel`
-    const response = await axios.get(url)
+    const response = await getData('nivel')
     const nivelList = Array.from(response.data)
     nivelList.forEach(function(nivel){
         nivelFilter.innerHTML+=`<option value="${nivel.description}">${nivel.description}</option>`
