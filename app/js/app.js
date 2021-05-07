484e5b1d0b90aa12d3802a1e1ae2c30560f78a84
LucasSpenthof <lucas.spenthof@gmail.com>
2021-05-06 20:57:32 -0400

    Import fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 import  getData  from "./api.js";
-import  getData  from "./rendercards.js";
+import  renderCards  from "./rendercards.js";
 
 const apiData = document.querySelector('.api-data')
 const spinner = document.querySelector('.spinner-grow')
@@ -29,7 +29,7 @@ async function search(query){
     showSpinner(false)
     const cursosList = Array.from(response.data)
     if(cursosList==""){
-        apiData.innerHTML='<div class="alert alert-warning"> Nenhum resultadi para ${query} </div>'
+        apiData.innerHTML='<div class="alert alert-warning"> Nenhum resultado para ${query} </div>'
 
     }else{
         apiData.innerHTML=""
